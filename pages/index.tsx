2bfef75f088fd381bf472b8df17c142b410a3681
pegasis <admin@pegasis.site>
2021-03-29 16:18:16 -0400

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+
+export default function Features() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Title</title>
+      </Head>
+    </div>
+  )
+}
